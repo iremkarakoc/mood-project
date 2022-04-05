af90ef0029e4debd44682a89f149ca95f550667f
İrem Karakoç <irem_karakoc52@hotmail.com>
2022-04-05 15:46:09 +0300

    yorum ekledik

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+//Test
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
